4f57aea4d4913751dd4a38d61d3be1c8425bb365
yskoht <ysk.oht@gmail.com>
2019-03-14 23:59:19 +0900

    fix bash/history.sh

--- bash/history.sh
+++ bash/history.sh
@@ -4,7 +4,7 @@
 export HISTSIZE=10000
 export HISTFILESIZE=10000
 export HISTCONTROL=ignoreboth:erasedups
-export HISTIGNORE="g st:g br*:cd*:ls*:la*:ll*"
+export HISTIGNORE="g st:g br*"
 
 function share_history {
     history -a
