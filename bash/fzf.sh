9c9d104cf4f04c2c21728ff799326c31442b5bf6
yskoht <ysk.oht@gmail.com>
2019-10-13 15:15:15 +0900

    add fzf scroll preview keybind and set preview-window width

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -8,7 +8,7 @@ fi
 # ---------------
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
 
-export FZF_DEFAULT_OPTS='--border --reverse --height 90%'
+export FZF_DEFAULT_OPTS='--border --reverse --height 90% --preview-window right:80% --bind ctrl-p:preview-up,ctrl-n:preview-down,ctrl-d:preview-page-down,ctrl-u:preview-page-up'
 
 # Key bindings
 # ------------
