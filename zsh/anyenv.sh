54c2031aa58ec5b1bed11f95a4aa37b208a03ee3
yskoht <ysk.oht@gmail.com>
2021-06-26 13:05:43 +0900

    fix anyenv

--- zsh/anyenv.sh
+++ zsh/anyenv.sh
@@ -1,2 +1,9 @@
 eval "$(anyenv init -)"
 
+export PYENV_ROOT="$HOME/.anyenv/envs/pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+
